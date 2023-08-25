225536f49f2372d4f618bfb8df75d83b234c9c47
Albert Colubi <albertmc89@gmail.com>
2023-08-25 17:06:50 +0200

    Create CustomError, endPointNotFound and generalErrorHandler

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,8 +1,15 @@
 import express from "express";
+import morgan from "morgan";
 import pingController from "../controllers/ping/pingController";
+import { endPointNotFound, generalErrorHandler } from "./middlewares/error";
 
 const app = express();
 
+app.use(morgan("dev"));
+
 app.use("/", pingController);
+
+app.use(endPointNotFound);
+app.use(generalErrorHandler);
 
 export default app;
