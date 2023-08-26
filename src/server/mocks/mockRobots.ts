17d2f7d55e7d99e51b3c54c22990d69de9282d60
Albert Colubi <albertmc89@gmail.com>
2023-08-26 13:43:38 +0200

    Create mock mockRobots

---
+++ src/server/mocks/mockRobots.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import { type RobotStructure } from "../../types";
+
+export const mockRobots: RobotStructure[] = [
+  {
+    id: new mongoose.Types.ObjectId().toString(),
+    name: "Chronodroid",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/8/84/Toyota_Robot_at_Toyota_Kaikan.jpg",
+    speed: 9,
+    endurance: 7,
+  },
+  {
+    id: new mongoose.Types.ObjectId().toString(),
+    name: "QuantaBot",
+    image:
+      "https://w7.pngwing.com/pngs/847/520/png-transparent-robot-action-toy-figures-character-action-fiction-robot-electronics-fictional-character-action-film.png",
+    speed: 6,
+    endurance: 4,
+  },
+];
