d9514cf5ffb971f6fdb658284af9b8f8e77b5a51
Albert Colubi <albertmc89@gmail.com>
2023-08-28 22:32:22 +0200

    Add addRobotController function with error handler

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,26 +2,32 @@ import cors from "cors";
 import "dotenv/config";
 import express from "express";
 import morgan from "morgan";
-import pingController from "../controllers/ping/pingController.js";
+import pingController from "./controllers/ping/pingController.js";
 import { endPointNotFound, generalErrorHandler } from "./middlewares/errors.js";
 import robotsRouter from "./routers/robotsRouters.js";
 
 const app = express();
 
 const corsOptions = {
-  origin: true,
+  origin: [
+    process.env.ALLOW_PROD_ORIGIN_HOME!,
+    process.env.ALLOW_PROD_ORIGIN_CREATE!,
+    process.env.ALLOW_LOCAL_ORIGIN_HOME!,
+    process.env.ALLOW_LOCAL_ORIGIN_CREATE!,
+  ],
   methods: "GET,POST",
   preflightContinue: false,
   optionsSuccessStatus: 204,
 };
 
 app.use(morgan("dev"));
+app.use(express.json());
 
 app.use(cors(corsOptions));
 
 app.use("/robots", robotsRouter);
 
-app.use("/", pingController);
+app.get("/", pingController);
 
 app.use(endPointNotFound);
 app.use(generalErrorHandler);
--- src/server/routers/robotsRouters.ts
+++ src/server/routers/robotsRouters.ts
@@ -1,11 +1,14 @@
 import cors from "cors";
 import express from "express";
-import { getRobotsController } from "../../controllers/robotsControllers.js";
+import { getRobotsController } from "../controllers/robotsControllers.js";
 
 const robotsRouter = express.Router();
 
 const optionsGet = {
-  origin: true,
+  origin: [
+    "https://202307-w7chwe-albert-moyano-front.netlify.app/",
+    "http://localhost:4007",
+  ],
   methods: "GET",
   preflightContinue: false,
   optionsSuccessStatus: 204,
